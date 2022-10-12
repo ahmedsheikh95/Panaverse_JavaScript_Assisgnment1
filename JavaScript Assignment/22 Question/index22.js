20fc85bce2119569aa1ca3cf912d85c3d2e377d4
ahmedsheikh95 <115325003+ahmedsheikh95@users.noreply.github.com>
2022-10-12 22:28:23 +0500

    Update index22.js

--- JavaScript Assignment/22 Question/index22.js
+++ JavaScript Assignment/22 Question/index22.js
@@ -1,6 +1,9 @@
 var arr = ["Car", "Motorcycle", "Bus", "Train"];
 for (var i = 0; i<5; i++) {
 console.log(arr[i]);
-if(i==4)
+if(i==6)
 console.log("Error Removed at index " + i)
 }
+
+arr.unshift("Jeep");
+console.log(arr);
