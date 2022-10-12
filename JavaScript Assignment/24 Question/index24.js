864d6724faee801a6669f617f6cc3d8caaa5785d
ahmedsheikh95 <115325003+ahmedsheikh95@users.noreply.github.com>
2022-10-12 15:10:17 +0500

    Update index24.js

--- JavaScript Assignment/24 Question/index24.js
+++ JavaScript Assignment/24 Question/index24.js
@@ -1,37 +1,39 @@
-var a = 3;
-found = false;
-if(a == 1){
-    console.log(found);
+var x = parseInt(prompt("Please enter the number"));
+console.log("X = " + x);
+var found = false;
+if(x === 5){
+    console.log("Conition met " +x);
+    found = true;
 }
-if(a == 3){
-console.log(found);
-}
+else
+console.log("Condition not met " + x);
 
-var text = 'GeEks FOR geeKS';
+
+var text = 'mY cOURse IS jaVAsCripT';
 
 console.log("Original String:");
 console.log(text);
 
-console.log("\nConverted String:");
+console.log("Converted String:");
 console.log(text.toLowerCase());
 
-var a = 3;
-var b = 3;
+var a = 8;
+var b = 8;
 
 console.log(a == b);
 console.log(a !== b);
 console.log(a <= b);
 console.log(a >= b);
 
-var a = 6;
-var b = 8;
+var a = 3;
+var b = 5;
 var c = a && b;
 var d = a || b ;
 console.log(c);
 console.log(d);
 
-var name = ["Saleem", "Ahmed", "Ammad", "Ahsan"];
-console.log("Ahmed is in Array: " + name.includes("Ahmed"));
+var name1 = ["Saleem", "Ahmed", "Ammad", "Ahsan"];
+console.log("Saleem is in Array: " + name1.includes("Saleem"));
 
-var name = ["Ali", "Ahmed", "Jawad", "Akber"];
-console.log("Iqbal is in Array: " + name.includes("Iqbal"));
+var name1 = ["Ali", "Ahmed", "Jawad", "Adeel"];
+console.log("Hassam is in Array: " + name1.includes("Hassam"));
