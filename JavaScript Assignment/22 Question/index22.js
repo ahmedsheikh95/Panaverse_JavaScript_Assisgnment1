b881ed6dd38658f6b47a94253acaf042b889c8bb
ahmedsheikh95 <115325003+ahmedsheikh95@users.noreply.github.com>
2022-10-12 22:28:58 +0500

    Update index22.js

--- JavaScript Assignment/22 Question/index22.js
+++ JavaScript Assignment/22 Question/index22.js
@@ -1,5 +1,5 @@
 var arr = ["Car", "Motorcycle", "Bus", "Train"];
-for (var i = 0; i<5; i++) {
+for (var i = 0; i<arr.lenght; i++) {
 console.log(arr[i]);
 if(i==6)
 console.log("Error Removed at index " + i)
