ade72ce593952c5b2fcac71286e6486eb82ce277
ahmedsheikh95 <115325003+ahmedsheikh95@users.noreply.github.com>
2022-10-12 21:38:26 +0500

    Update index19.js

--- JavaScript Assignment/19 Question/index19.js
+++ JavaScript Assignment/19 Question/index19.js
@@ -1,2 +1,6 @@
-var guest = ["Ahmed", "Ammad", "Ahsan", "Naila", "Sumbal"];
-console.log("Two Member are inviting inthe dinner i.e"  +" " +   guest[0]+ " &  " +guest[1]);
+var name = ["Ammad", "Saleem", "Ahmed"];
+name.unshift("Subhan");
+   name.push("Umer")
+ for(let i = 0; i < name.length; i++){
+    console.log("Hello " + name[i] +  "! This is an Invitation Message for you to Dinner, Please come and Join us on Sunday Oct 12th, 2022");
+    console.log("We are Inviting " + name.length + " people for Dinner");
