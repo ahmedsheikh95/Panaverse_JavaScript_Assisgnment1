a098ad81de87f3f99192f470635367a354d9a3a3
ahmedsheikh95 <115325003+ahmedsheikh95@users.noreply.github.com>
2022-10-12 15:08:57 +0500

    Update index17.js

--- JavaScript Assignment/17 Question/index17.js
+++ JavaScript Assignment/17 Question/index17.js
@@ -1,23 +1,14 @@
 let Name = ["Ahmed", "Ammad", "Ahsan"];
-Name.unshift("Naila");
-    Name.push("Sumbal");
-//for(let i = 0; i < 5; i++){
-    //Name.unshift("Naila");
-    //Name.push("Sumbal");
-  //console.log("Hello " + Name[i] +  " This Is The Invitation Message For you to Dinner, Please come and Join Us at Sunday 08 Oct, 2022");
-//Name[2]="Sumbal";
-//}
+Name.unshift("Nagina");
+    Name.push("Hassam");
+console.log(Name);
+console.log("Sorry " + Name + " Our dining table is not delivered yet");
 let NewName = Name.slice(1 , 3);
 console.log(NewName);
 console.log("We can only invite two person")
-//NewName = [];
-//console.log(NewName + "We Can't Invite you all in Dinner");
-for(let i = 0; i < 2; i++){
-    //Name.unshift("Naila");
-    //Name.push("Sumbal");
+for(let i = 0; i < NewName.length; i++){
   console.log("Hello " + NewName[i] +  " This Is The Invitation Message For you to Dinner, Please come and Join Us at Sunday 08 Oct, 2022");
-//Name[2]="Sumbal";
 }
-
+console.log(NewName);
 NewName = [];
 console.log(NewName);
