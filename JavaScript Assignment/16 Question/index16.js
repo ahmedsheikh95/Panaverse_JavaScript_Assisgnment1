3d6e513487e10809011b7c3e1df93599507e44ef
ahmedsheikh95 <115325003+ahmedsheikh95@users.noreply.github.com>
2022-10-12 15:07:50 +0500

    Update index16.js

--- JavaScript Assignment/16 Question/index16.js
+++ JavaScript Assignment/16 Question/index16.js
@@ -1,10 +1,9 @@
 let Name = ["Ahmed", "Ammad", "Ahsan"];
-Name.unshift("Naila");
-    Name.push("Sumbal");
-for(let i = 0; i < 5; i++){
-    //Name.unshift("Naila");
-    //Name.push("Sumbal");
+console.log("Hello " + Name + " We just have recieved our bigger dining table and have more space to invite friends for dinner");
+Name.unshift("Nagina");
+Name.splice(2,0, "Usman");
+Name.push("Hassam");
+for(let i = 0; i < 6; i++){
   console.log("Hello " + Name[i] +  " This Is The Invitation Message For you to Dinner, Please come and Join Us at Sunday 08 Oct, 2022");
-//Name[2]="Sumbal";
 
 }
