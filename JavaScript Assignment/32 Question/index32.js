eb3e218aadfa36717bbcb8c3644ffa0e3d48ebd1
ahmedsheikh95 <115325003+ahmedsheikh95@users.noreply.github.com>
2022-10-12 15:11:24 +0500

    Update index32.js

--- JavaScript Assignment/32 Question/index32.js
+++ JavaScript Assignment/32 Question/index32.js
@@ -1,12 +1,14 @@
-var current_users = ['John', 'Mark', 'Dan', 'Ria', 'Ben']; 
-var new_users = ['Mark', 'Shia', 'Shan', 'Ria', 'Shelly'];
-var found = false;
-for(i=0; i<current_users.length; i++){
-    for(j=0; j<new_users.length; j++)
-if(new_users[j] !== current_users[i]){
-console.log("Great, " + new_users[j] + " Username is available");
+let current_users = ['Ahmed', 'Ammad', 'Saleem', 'Iqbal', 'Hassam']; 
+let new_users = ['Nagina', 'Saleem', 'John', 'Hassam', 'Ammad'];
+let found = false;
+for(i=0; i < current_users.length; i++){
+    for(j=0; j < new_users.length; j++)
+if(new_users[j] === current_users[i]){
+console.log("Hello " +new_users[j]+ " you need to enter a new username");
 found = true;
 break;
 }}
-if(found)
-console.log("Hello " +new_users[j]+ " you need to enter a new username");
+if(found){
+console.log("Great, " + new_users[0] + " Username is available");
+console.log("Great, " + new_users[2] + " Username is available");
+}
