12a7da7093258b53b817fdeca41286fbc5f9577a
ahmedsheikh95 <115325003+ahmedsheikh95@users.noreply.github.com>
2022-10-12 15:05:33 +0500

    Update index15.js

--- JavaScript Assignment/15 Question/index15.js
+++ JavaScript Assignment/15 Question/index15.js
@@ -1,6 +1,6 @@
 let Name = ["Ahmed", "Ammad", "Ahsan"];
-
+console.log("Sorry " + Name[1] + " is not able to join us for dinner");
 for(let i = 0; i < Name.length; i++){
   console.log("Hello " + Name[i] +  " This Is The Invitation Message For you to Dinner, Please come and Join Us at Sunday 08 Oct, 2022");
-Name[2]="Sumbal";
+Name[1]="Hassam";
 }
